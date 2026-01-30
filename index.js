6ecd4ab8e2da7ecb03bb9ea39b231c567d503ba1
Naunidh Singh <naunidhw09@gmail.com>
2026-01-29 19:48:38 -0500

    added delte route, marks the end of project

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port =8080;
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override")
 
 const path = require("path")
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -41,10 +43,11 @@ app.post("/posts", (req,res) => {
 app.patch("/posts/:id", (req,res) =>{
     let {id} = req.params;
     let newcontent = req.body.content;
-    let post = post.find((p) => id == p.id);
+    let post = posts.find((p) => id == p.id);
     post.content = newcontent;
     console.log(post);
-   res.send("working")
+//    res.send("patch working");
+   res.redirect("/posts")
 })
 
 app.get("/posts/:id/edit" ,(req,res)=>{
@@ -52,6 +55,12 @@ app.get("/posts/:id/edit" ,(req,res)=>{
     let post = posts.find((p) => id == p.id);
     res.render("edit.ejs");
 });
+
+app.delete("/posts/:id", (req,res) =>{
+    let {id} = req.params;
+    posts = posts.filter((p) => p.id != id);
+    res.redirect("/posts");
+})
 
 let posts = [
   { id : uuidv4(),
